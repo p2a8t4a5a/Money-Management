2224fc05637c954684cda4ba78d64269eec91243
Daniel194 <lungu.daniel94@gmail.com>
2017-10-07 22:48:19 +0300

    Script file to start application locally

---
+++ start_dev.sh
@@ -0,0 +1,6 @@
+export CONFIG_SERVICE_PASSWORD="test123"
+export STATISTICS_SERVICE_PASSWORD="test123"
+export NOTIFICATION_SERVICE_PASSWORD="test123"
+export ACCOUNT_SERVICE_PASSWORD="test123"
+export MONGODB_PASSWORD="test123"
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml up
