f7a64ec525f4fa2e5810440d2f2213a34252ffe3
Daniel194 <lungu.daniel94@gmail.com>
2018-03-11 00:43:27 +0200

    account-service -> fix dependency problems

--- account-service/src/main/java/com/money/management/account/client/AuthServiceClient.java
+++ account-service/src/main/java/com/money/management/account/client/AuthServiceClient.java
@@ -1,7 +1,7 @@
 package com.money.management.account.client;
 
 import com.money.management.account.domain.User;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
