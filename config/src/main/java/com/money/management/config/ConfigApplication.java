4089ed560a5a769964bf82a23fd6c2bb019b925d
Daniel194 <lungu.daniel94@gmail.com>
2018-05-18 08:50:20 +0300

    config -> change configurations

--- config/src/main/java/com/money/management/config/ConfigApplication.java
+++ config/src/main/java/com/money/management/config/ConfigApplication.java
@@ -2,13 +2,15 @@ package com.money.management.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
+@SpringBootApplication
 public class ConfigApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
 }
