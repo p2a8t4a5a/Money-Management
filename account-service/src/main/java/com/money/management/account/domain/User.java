c2243aa0332078ca9beb1f1e1424174e9360151c
Daniel194 <lungu.daniel94@gmail.com>
2018-06-11 22:51:56 +0300

    account-service -> improvements

--- account-service/src/main/java/com/money/management/account/domain/User.java
+++ account-service/src/main/java/com/money/management/account/domain/User.java
@@ -2,31 +2,32 @@ package com.money.management.account.domain;
 
 import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 
 public class User {
 
-	@NotNull
-	@Length(min = 3, max = 20)
-	private String username;
+    @Email
+    @Length(min = 3, max = 64)
+    private String username;
 
-	@NotNull
-	@Length(min = 6, max = 40)
-	private String password;
+    @NotNull
+    @Length(min = 6, max = 40)
+    private String password;
 
-	public String getUsername() {
-		return username;
-	}
+    public String getUsername() {
+        return username;
+    }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
