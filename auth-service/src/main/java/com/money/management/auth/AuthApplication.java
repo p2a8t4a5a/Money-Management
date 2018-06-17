94eeb564238217a624ddb6b0fc38e415dfc2bb6c
Daniel194 <lungu.daniel94@gmail.com>
2018-06-17 19:51:32 +0300

    minor fixes

--- auth-service/src/main/java/com/money/management/auth/AuthApplication.java
+++ auth-service/src/main/java/com/money/management/auth/AuthApplication.java
@@ -106,7 +106,7 @@ public class AuthApplication {
         }
 
         @Override
-        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
             endpoints
                     .tokenStore(tokenStore)
                     .authenticationManager(authenticationManager)
@@ -114,7 +114,7 @@ public class AuthApplication {
         }
 
         @Override
-        public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+        public void configure(AuthorizationServerSecurityConfigurer oauthServer) {
             oauthServer
                     .tokenKeyAccess("permitAll()")
                     .checkTokenAccess("isAuthenticated()");
