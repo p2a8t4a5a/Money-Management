e4852d2b74d2649ec1b8c6bd7bca681a11bc7ead
Daniel194 <lungu.daniel94@gmail.com>
2018-06-27 00:31:34 +0300

    notification-service -> change user auth

--- notification-service/src/main/java/com/money/management/notification/client/AccountServiceClient.java
+++ notification-service/src/main/java/com/money/management/notification/client/AccountServiceClient.java
@@ -2,14 +2,14 @@ package com.money.management.notification.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "account-service")
 public interface AccountServiceClient {
 
-	@RequestMapping(method = RequestMethod.GET, value = "/accounts/{accountName}", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	String getAccount(@PathVariable("accountName") String accountName);
+    @RequestMapping(method = RequestMethod.GET, value = "/accounts/find", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    String getAccount(@RequestParam("name") String accountName);
 
 }
