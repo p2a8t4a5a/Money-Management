c88fc8a3d3ae6199b1b79e9740db87cd4b6344da
Daniel194 <lungu.daniel94@gmail.com>
2018-07-04 23:02:28 +0300

    user-interface -> cleanup and add router module

--- user-interface/src/app/app.module.ts
+++ user-interface/src/app/app.module.ts
@@ -1,18 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './login/login.component';
+import {RouterModule} from "@angular/router";
+import {CookieService} from "ngx-cookie-service";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LoginComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot([
+            {path: 'login', component: LoginComponent}
+        ])
+    ],
+    providers: [CookieService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
