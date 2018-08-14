370ccce0d56e755c5ab0c02bda4fcee5432613e6
Daniel194 <lungu.daniel94@gmail.com>
2018-08-14 23:39:30 +0300

    user-interface -> login page - change wow animation to angular animation

--- user-interface/src/app/login/login.component.ts
+++ user-interface/src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, ChangeDetectorRef, Component, HostListener, OnInit} from '@angular/core';
 import {animate, state, style, transition, trigger} from '@angular/animations';
 import {User} from '../domain/User';
 import {FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators} from "@angular/forms";
@@ -19,20 +19,44 @@ import {SnackBarComponent} from "../snack-bar/snack-bar.component";
             })),
             transition('active => inactive', animate('1000ms ease-out')),
             transition('inactive => active', animate('1000ms ease-in'))
+        ]),
+        trigger('showLogin', [
+            state('true', style({opacity: 1, transform: 'translateY(0%)'})),
+            state('false', style({opacity: 0, transform: 'translateY(100%)'})),
+            transition('false => true', [animate('1s 0.5s ease-in')])
+        ]),
+        trigger('showBar', [
+            state('true', style({opacity: 1, transform: 'translateY(0%)'})),
+            state('false', style({opacity: 0, transform: 'translateY(25%)'})),
+            transition('false => true', animate('1s 1s ease-in'))
+        ]),
+        trigger('showSocial', [
+            state('true', style({opacity: 1, transform: 'translateY(0%)'})),
+            state('false', style({opacity: 0, transform: 'translateY(25%)'})),
+            transition('false => true', animate('1s 2s ease-in'))
         ])
     ]
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, AfterViewInit {
     public user: User;
+
     public hidePassword1: Boolean;
     public hidePassword2: Boolean;
     public hidePassword3: Boolean;
+
     public flip: String;
+
     public loginForm: FormGroup;
     public createAccountForm: FormGroup;
+
     public matcher: PasswordErrorStateMatcher;
 
-    constructor(private fb: FormBuilder, private snackbar: MatSnackBar) {
+    public showSection: Boolean = false;
+
+    private windowHeight: number;
+    private scrollPosition: number;
+
+    constructor(private fb: FormBuilder, private snackbar: MatSnackBar, private cdr: ChangeDetectorRef) {
         this.user = new User();
         this.flip = 'inactive';
         this.hidePassword1 = true;
@@ -53,6 +77,27 @@ export class LoginComponent implements OnInit {
     }
 
     ngOnInit() {
+        this.windowHeight = window.innerHeight;
+    }
+
+    ngAfterViewInit(): void {
+        this.scrollPosition = window.pageYOffset;
+        this.checkShowSection();
+        this.cdr.detectChanges();
+    }
+
+    @HostListener('window:scroll', ['$event'])
+    checkScroll() {
+        this.scrollPosition = window.pageYOffset;
+        this.checkShowSection();
+    }
+
+    @HostListener('window:resize', ['$event'])
+    onResize() {
+        this.scrollPosition = window.pageYOffset;
+        this.windowHeight = window.innerHeight;
+
+        this.checkShowSection();
     }
 
     onSubmitCreateAccount() {
@@ -75,6 +120,12 @@ export class LoginComponent implements OnInit {
         let confirmPass = group.controls.repeatPassword.value;
 
         return pass === confirmPass ? null : {notSame: true}
+    }
+
+    private checkShowSection() {
+        if (this.windowHeight * 4.5 <= this.scrollPosition) {
+            this.showSection = true;
+        }
     }
 
 }
