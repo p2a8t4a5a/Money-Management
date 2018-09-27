d92e3902c3280ecd6d6e9bc3983a0542e181a3a9
Daniel194 <lungu.daniel94@gmail.com>
2018-09-27 22:58:45 +0300

    user-interface -> income popup window - integrate item domain in the interface

---
+++ user-interface/src/app/domain/Item.ts
@@ -0,0 +1,7 @@
+export class Item {
+    title: String;
+    amount: String;
+    currency: String;
+    period: String;
+    icon: String;
+}
--- user-interface/src/app/income-dialog/income-dialog.component.ts
+++ user-interface/src/app/income-dialog/income-dialog.component.ts
@@ -1,5 +1,6 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
+import {Item} from "../domain/Item";
 
 @Component({
     selector: 'app-income-dialog',
@@ -8,9 +9,17 @@ import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
 })
 export class IncomeDialogComponent implements OnInit {
 
+    item: Item = new Item();
+
     constructor(
         public dialogRef: MatDialogRef<IncomeDialogComponent>,
         @Inject(MAT_DIALOG_DATA) public data: String) {
+    }
+
+    ngOnInit() {
+        this.item.currency = 'EUR';
+        this.item.period = 'MONTH';
+        this.item.amount = '0';
     }
 
     onCloseClick(): void {
@@ -18,7 +27,8 @@ export class IncomeDialogComponent implements OnInit {
         this.dialogRef.close();
     }
 
-    ngOnInit() {
+    onAddClick() {
+        //TODO
     }
 
 }
