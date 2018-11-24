849d699f5693275c24ef237200837577aff0cb13
Daniel194 <lungu.daniel94@gmail.com>
2018-11-24 21:54:28 +0200

    user-interface -> statistic page - transfer all the old function to the new format - WIP

--- user-interface/src/app/statistics/statistics.component.ts
+++ user-interface/src/app/statistics/statistics.component.ts
@@ -3,6 +3,7 @@ import {AuthenticationService} from "../service/authentication.service";
 import {Router} from "@angular/router";
 import {StatisticsService} from "../service/statistics.service";
 import {DataPoint} from "../domain/DataPoint";
+import {$} from "jquery";
 
 @Component({
     selector: 'app-statistics',
@@ -22,6 +23,9 @@ export class StatisticsComponent implements OnInit {
         this.statisticsService.getCurrentAccountStatistics().subscribe(result => {
             this.dataPoints = result;
         });
+
+        this.createMainCanvas();
+        this.createLittleCanvases();
     }
 
     public logout() {
@@ -30,6 +34,96 @@ export class StatisticsComponent implements OnInit {
 
     public navigateToAccount() {
         this.router.navigate(['/account']);
+    }
+
+    private createLittleCanvases() {
+        let canvas1 = $("#movingcircle-1"),
+            canvas2 = $("#movingcircle-2"),
+            canvas3 = $("#movingcircle-3"),
+            ctx1 = canvas1.getContext('2d'),
+            ctx2 = canvas2.getContext('2d'),
+            ctx3 = canvas3.getContext('2d'),
+            x = canvas1.width / 2,
+            y = canvas1.height / 2,
+            radius = 99,
+            startAngle = Math.PI,
+            endAngle = 2.2 * Math.PI,
+            counterClockwise = false;
+
+        ctx1.beginPath();
+        ctx1.arc(x, y, radius, startAngle, endAngle, counterClockwise);
+        ctx1.lineWidth = 3;
+        ctx1.strokeStyle = "#f2f2f2";
+        ctx1.stroke();
+
+        ctx2.beginPath();
+        ctx2.arc(x, y, radius, startAngle, endAngle, counterClockwise);
+        ctx2.lineWidth = 3;
+        ctx2.strokeStyle = "#f2f2f2";
+        ctx2.stroke();
+
+        ctx3.beginPath();
+        ctx3.arc(x, y, radius, startAngle, endAngle, counterClockwise);
+        ctx3.lineWidth = 3;
+        ctx3.strokeStyle = "#f2f2f2";
+        ctx3.stroke();
+    }
+
+    private createMainCanvas() {
+        let canvas = $("#horizontal"),
+            currentDate = new Date(),
+            currentMonth = currentDate.getMonth(),
+            allMonths = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        let savingsChart = $('#savingschart');
+
+        if (canvas.getContext) {
+            var ctx = canvas.getContext("2d");
+            ctx.canvas.width = savingsChart.width();
+            ctx.canvas.height = savingsChart.height();
+            for (let i = 0; i < 11; i++) {
+                ctx.beginPath();
+                ctx.moveTo(0, 1 + i * 22);
+                ctx.lineTo(savingsChart.width(), 1 + i * 22);
+                ctx.strokeStyle = '#e5e5e5';
+                ctx.stroke();
+                ctx.beginPath();
+                ctx.moveTo(0, 0.5 + i * 22);
+                ctx.lineTo(savingsChart.width(), 0.5 + i * 22);
+                ctx.strokeStyle = '#FFF';
+                ctx.stroke();
+            }
+        }
+
+        for (let i = 0, j = currentMonth; i <= 12; i++, j++) {
+            if (j == 12) {
+                j = 0
+            }
+            $("#month" + i).children(".month-name").html(allMonths[j]);
+        }
+    }
+
+    private drawChartLine(position) {
+        let canvas = $('#chartline');
+        let savingsChart = $('#savingschart');
+
+        let chartWidth = savingsChart.width(),
+            chartHeight = savingsChart.height(),
+            segmentWidth = $(".months").width();
+
+        if (canvas.getContext) {
+            var ctx = canvas.getContext("2d");
+            ctx.canvas.width = savingsChart.width();
+            ctx.canvas.height = savingsChart.height();
+            ctx.beginPath();
+            ctx.moveTo(0, 155);
+            ctx.lineTo(savingsChart.width(), position);
+            ctx.strokeStyle = '#bed8db';
+            ctx.stroke();
+        }
+
+        for (let i = 0, j = 12; i < 12; i++, j--) {
+            $('#month' + j).css({"height": chartHeight - position - i * 7 - ((i * segmentWidth * (chartHeight - 153 - position)) / chartWidth)});
+        }
     }
 
 }
