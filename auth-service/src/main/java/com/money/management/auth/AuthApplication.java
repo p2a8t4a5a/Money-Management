cc39f43193efe9d09568bdb5d1766c6f26f50715
Daniel194 <lungu.daniel94@gmail.com>
2019-01-30 20:19:11 +0200

    auth-service -> take out the security from resend verification email endpoint.

--- auth-service/src/main/java/com/money/management/auth/AuthApplication.java
+++ auth-service/src/main/java/com/money/management/auth/AuthApplication.java
@@ -52,7 +52,7 @@ public class AuthApplication {
 
         @Override
         public void configure(WebSecurity webSecurity) {
-            webSecurity.ignoring().antMatchers("/users/verification**");
+            webSecurity.ignoring().antMatchers("/users/verification**", "/users/verification/resend**");
         }
 
         @Override
